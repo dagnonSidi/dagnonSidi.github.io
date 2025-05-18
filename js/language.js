114916030d87a6ac0c9d09c51e3450e1acfa3563
dagnonSidi <s.sidi.dagnon@gmail.com>
2025-05-18 13:55:13 +0200

    Update language.js

--- js/language.js
+++ js/language.js
@@ -39,8 +39,8 @@ function updateContent() {
 
     // Update meta tags
     document.title = currentLanguage === 'fr' ? 
-        'Gueguen Yann - Portfolio' : 
-        'Gueguen Yann - Portfolio';
+        'DAGNON sidi - Portfolio' : 
+        'DAGNON sidi - Portfolio';
 }
 
 // Initialize language on page load
